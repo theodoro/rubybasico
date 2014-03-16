ecbe089d903c8098cad6124f844b537f8474a50f
Bruno Aquino <theodorodeaquino@gmail.com>
2014-03-16 17:05:59 -0300

    Primeiro commit estudos de Ruby

---
+++ calc.rb
@@ -0,0 +1,21 @@
+# Escreve na tela tudo que vem depois dele
+puts 1  + 2 
+
+#Aritmética Simples
+
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+puts "------------------------------------"
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+puts "------------------------------------"
+
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8)) * -52
+
+
---
+++ letra.rb
@@ -0,0 +1,19 @@
+puts 'Olá, mundo'
+puts ''
+puts 'Até logo.'
+
+puts "Aritimética das Strings"
+
+puts 'Eu gosto de' + 'torta de maçã.'
+puts 'Eu gosto de ' + 'torta de maçã'
+
+# Multiplicação de String
+puts 'piscar ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5' 
